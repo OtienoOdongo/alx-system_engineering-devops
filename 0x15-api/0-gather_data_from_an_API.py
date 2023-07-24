19da53a35ea49bdb71a183aeb20b2143d5e40b4a
OtienoOdongo <otabiznake@gmail.com>
2023-07-24 15:47:02 -0700

    GET request via REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,11 +12,18 @@ base_url = 'https://jsonplaceholder.typicode.com/'
 
 if __name__ == '__main__':
     employee_id = sys.argv[1]
-    user_response = requests.get(base_url + 'users/{}'.format(employee_id)).json()
-    todo_response = requests.get(base_url + 'todos', params={'userId': employee_id}).json()
+    user_response = requests.get(
+            base_url + 'users/{}'.format(employee_id)
+            ).json()
+    todo_response = requests.get(
+            base_url + 'todos', params={'userId': employee_id}
+            ).json()
 
-    completed_tasks = [task['title'] for task in todo_response if task['completed']]
+    completed_tasks = [
+            task['title'] for task in todo_response if task['completed']
+            ]
     total_tasks = len(todo_response)
 
-    print(f"Employee {user_response['name']} is done with tasks({len(completed_tasks)}/{total_tasks}): ")
+    print(f"Employee {user_response['name']} is done with tasks ", end="")
+    print(f"({len(completed_tasks)}/{total_tasks}):")
     print("\n".join(f"\t{task}" for task in completed_tasks))
