39ff1822b4cbd803f796f188c2e154896d841031
OtienoOdongo <otabiznake@gmail.com>
2023-07-26 16:43:33 -0700

    exporting all employees data to json

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,20 +1,39 @@
 #!/usr/bin/python3
 """
-exporting all employee information in json format
+Exporting all employee information in JSON format.
 """
+
 import json
 import requests
+
+
+def get_user_tasks(user_id, url):
+    todo_response = requests.get(url + "todos",
+                                 params={"userId": user_id}).json()
+    tasks = [
+        {
+            "task": todo.get("title"),
+            "completed": todo.get("completed")
+        }
+        for todo in todo_response
+    ]
+    return tasks
+
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
 
+    all_users_tasks = {}
+
+    for user in users:
+        user_id = user.get("id")
+        username = user.get("username")
+        tasks = get_user_tasks(user_id, url)
+        all_users_tasks[user_id] = {
+            "username": username,
+            "tasks": tasks
+        }
+
     with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            user.get("id"): [{
-                "task": todo.get("title"),
-                "completed": todo.get("completed"),
-                "username": user.get("username")
-            } for todo in requests.get(url + "todos",
-                                       params={"userId": u.get("id")}).json()]
-            for user in users}, jsonfile)
+        json.dump(all_users_tasks, jsonfile, indent=2)
