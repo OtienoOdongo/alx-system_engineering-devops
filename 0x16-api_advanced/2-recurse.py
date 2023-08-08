a1524d7f3fffd958cfb5b9fe050cdf02c3547f69
OtienoOdongo <otabiznake@gmail.com>
2023-08-08 13:55:18 -0700

    use recursive function in API

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,18 +7,21 @@ list of titles all hot articles
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=None):
     """
     A Recursive function that queries Reddit API
-    and returns a list titles of all hot articles for a given subreddit
+    and returns a list of titles of all hot articles for a given subreddit
     """
+    if hot_list is None:
+        hot_list = []
+
     if not is_valid_subreddit(subreddit):
-        return None
+        return []
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "Google chrome version  115.0.5790.11"}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers)
 
     # Checking if the request was successful
     if response.status_code == 200:
@@ -31,7 +34,8 @@ def recurse(subreddit, hot_list=[]):
             hot_list.append(title)
 
         # Check if there are more pages to fetch
-        if data['data']['after'] is not None:
+        after = data['data']['after']
+        if after is not None:
             # Recursive call to fetch the next page
             recurse(subreddit, hot_list)
 
@@ -39,8 +43,10 @@ def recurse(subreddit, hot_list=[]):
 
 
 def is_valid_subreddit(subreddit):
-    # Make a request to the Reddit API to check if the subreddit exists
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Google chrome version  115.0.5790.11"}
+
+    response = requests.get(url, headers=headers)
 
     # Check if the request was successful and the subreddit exists
     if response.status_code == 200:
